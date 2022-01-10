513a1dcaf88cd8ae6a305c7b87a562a9b59a739e
SonOfCrypto <crypto@janwagner-design.de>
2022-01-10 10:12:13 +0100

    fixed double loading of dialog template

--- app/js/init.js
+++ app/js/init.js
@@ -23,36 +23,32 @@ $(document).ready(async () => {
     $("#init-bottombar").load("templates/bottombar.html", function() {
         $("#bottombar").unwrap();
     });
-    $("#init-overlay").load("templates/overlay.html", function() {
-        var overlay = $("#overlay");
-        $(overlay).unwrap();
-        $(overlay).load("./templates/overlay.html", async () => {
-            $('#chain_id').text(CONFIG.chain_id);
-            if(close_screen_metamask_if_complete()) {
-                if(await close_screen_network_if_complete()) {
+    $("#init-overlay").load("./templates/overlay.html", async () => {
+        $("#overlay").unwrap();
+        $('#chain_id').text(CONFIG.chain_id);
+        if(close_screen_metamask_if_complete()) {
+            if(await close_screen_network_if_complete()) {
 
-                    contract_accounts = new ContractAccounts(await contract_posts.get_accounts_address());
+                contract_accounts = new ContractAccounts(await contract_posts.get_accounts_address());
 
-                    await set_post_fetcher();
-                    update_feed();
-                    setInterval( update_feed, 5000);
+                await set_post_fetcher();
+                update_feed();
+                setInterval( update_feed, 5000);
 
-                    get_username().then(username => {
-                        let profile_username = get_profile_username();
-                        if(profile_username && username !== profile_username) {
-                            $('#input').css("display", "none");
-                            $('#feed > .container').prepend($("<div class='text-center text-xl font-bold mb-5'></div>").addClass("profile_title").text(profile_username));
-                        }
+                get_username().then(username => {
+                    let profile_username = get_profile_username();
+                    if(profile_username && username !== profile_username) {
+                        $('#input').css("display", "none");
+                        $('#feed > .container').prepend($("<div class='text-center text-xl font-bold mb-5'></div>").addClass("profile_title").text(profile_username));
+                    }
 
-                        $('#profile').attr('href', "?u="+username);
-                    });
+                    $('#profile').attr('href', "?u="+username);
+                });
 
-                    close_screen_signup_if_complete();
-                }
+                close_screen_signup_if_complete();
             }
-
-            $('body').css("opacity", "1");
-        });
+        }
+        $('body').css("opacity", "1");
     });
     // $("#init-dialog").load("templates/dialog.html", function() {
     //     $("#dialog").unwrap();
