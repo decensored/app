f51192093bb9c4f79ce419498a53eb927908827a
SonOfCrypto <crypto@janwagner-design.de>
2022-01-16 19:56:27 +0100

    reverted config.js

--- app/config.js
+++ app/config.js
@@ -1,7 +1,5 @@
 const CONFIG = {
     chain_id: 1075,
-    evm_node: "https://we.jamesayden.com",
-    contract_posts_address: "0xF79F25fCbb3600f3641b73f3cbd9eaE3c0C8B4c0",
-    whitelist: "./whitelist",
-    blacklist: "./blacklist"
+    evm_node: "https://we.addiota.com",
+    contract_posts_address: "0x63A90a1Ce9779282709898c1bd88326c3F810014"
 }
