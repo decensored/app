1ca9fe176c5c5c9b4267a5bb52b5eedf68d2a177
Eric van Riet Paap <ericvrp@gmail.com>
2022-01-20 16:18:02 +0100

    copy images from v1 to v2 and pick correct favicon

--- v2/pages/index.tsx
+++ v2/pages/index.tsx
@@ -8,7 +8,23 @@ const Home: NextPage = () => {
       <Head>
         <title>Decensored</title>
         <meta name="description" content="Decensored" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon/favicon-16x16.png"
+        />
       </Head>
 
       <main>
