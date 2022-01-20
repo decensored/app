ae2b3c616029109ac86951701098fd304fdeff20
Eric van Riet Paap <ericvrp@gmail.com>
2022-01-20 15:43:15 +0100

    support baseUrl for easier importing deeply nested files

--- v2/pages/index.tsx
+++ v2/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import HelloWorld from '../components/HelloWorld'
+import HelloWorld from 'components/HelloWorld'
 
 const Home: NextPage = () => {
   return (
