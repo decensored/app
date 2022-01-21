a48831e6f043a0097066334df091398df886e1a5
christopherturinsky <christopher@myclubs.com>
2022-01-21 14:45:38 +0100

    fix: add more validation for space creation

--- pages/spaces/spaces.js
+++ pages/spaces/spaces.js
@@ -6,10 +6,10 @@ function on_create_space_button_clicked() {
 }
 
 async function on_create_space_submit_button_pressed() {
-    if(await is_signed_up()) {
+    if (await is_signed_up()) {
         const name = $('#create_space_name_input').val();
-        check_for_invalid_input(name, /[\s]/, 0, 30).then(function(result) {
-            if($.isEmptyObject(result)) {
+        check_for_invalid_input(name, /[^a-z\s]/, 0, 30).then(function (result) {
+            if ($.isEmptyObject(result)) {
                 create_space(name).then(() => {
                     window.location.href = BASE_URL + '?s=' + name
                 });
@@ -18,7 +18,7 @@ async function on_create_space_submit_button_pressed() {
                     result.pattern,
                     result.minLength,
                     result.maxLength
-                ).then(function(message) {
+                ).then(function (message) {
                     alert(message)
                 });
             };
@@ -44,9 +44,9 @@ function toggle_space_dialog() {
 
 async function load_data_for_spaces() {
     const latest = await get_latest_space_index();
-    for(let i = latest; i > 0; i--) {
+    for (let i = latest; i > 0; i--) {
         get_space(i).then(space => {
-            if (!space.name) return; 
+            if (!space.name) return;
             append_space_to_view(space);
         });
     }
@@ -60,21 +60,21 @@ function create_one_space_element(space) {
     const count = Math.floor(Math.random() * 200);
     const dummyDescription = 'Bacon ipsum dolor amet fatback cupim drumstick flank, salami cow sirloin prosciutto shoulder pork chop beef sausage ham hock spare ribs.';
     var spaceTemplate = [
-        '<div class="test bg-white dark:bg-gray-900 rounded shadow-sm" data-name="' + space.name + '" style="order: '+-space.id+'">',
+        '<div class="bg-white dark:bg-gray-900 rounded shadow-sm" data-name="' + space.name + '" style="order: ' + -space.id + '">',
         /*'<img src="' + img +'" class="w-full h-60"></img>',*/
-            '<div class="rounded-t p-5">',
-                '<div class="meta flex justify-between">',
-                    '<a href="?s=' + space.name + '" class="font-bold text-gray-900 dark:text-gray-300">',
-                        '<i class="fas fa-satellite mr-2"></i>' +space.name,
-                    '</a>',
-                    '<div class="members flex justify-end items-center gap-x-2 pointer-events-none">',
-                        '<i class="fas fa-user-astronaut text-md text-gray-900 dark:text-gray-300" title="Members"></i>',
-                        '<span class="flex-none uppercase bg-decensored-100 text-gray-600 text-xs tracking-wide font-semibold px-2 py-1 rounded-md">' + count + '</span>',
-                    '</div>',
-                '</div>',
-                '<div class="message break-words mt-2">' + dummyDescription + '</div>',
-            '</div>',
-        '</div>'
+        '<div class="rounded-t p-5">',
+        '<div class="meta flex justify-between">',
+        '<a href="?s=' + space.name + '" class="font-bold text-gray-900 dark:text-gray-300">',
+        '<i class="fas fa-satellite mr-2"></i>' + space.name,
+        '</a>',
+        '<div class="members flex justify-end items-center gap-x-2 pointer-events-none">',
+        '<i class="fas fa-user-astronaut text-md text-gray-900 dark:text-gray-300" title="Members"></i>',
+        '<span class="flex-none uppercase bg-decensored-100 text-gray-600 text-xs tracking-wide font-semibold px-2 py-1 rounded-md">' + count + '</span>',
+        '</div>',
+        '</div>',
+        '<div class="message break-words mt-2">' + dummyDescription + '</div>',
+        '</div>',
+        '</div>',
     ];
     return $(spaceTemplate.join(''));
 }
@@ -82,17 +82,17 @@ function create_one_space_element(space) {
 // ONE SPACE
 async function load_data_for_space() {
     const name = get_url_param("s");
-    if(name) {
+    if (name) {
         get_space_id(name).then(space => {
-            if(space && space.id != 0) {
+            if (space && space.id != 0) {
                 $("#space_name").html(space.name);
                 $("#space_description").html("LOREM");
-                $("#submit_space_post").attr("onclick", "submit_post_input("+space.id+")");
-                $("#update_space").attr("onclick", "update_feed("+space.id+")");
+                $("#submit_space_post").attr("onclick", "submit_post_input(" + space.id + ")");
+                $("#update_space").attr("onclick", "update_feed(" + space.id + ")");
                 update_feed(space.id);
             } else {
                 window.location.href = BASE_URL + '?p=spaces';
             }
         })
-    } 
+    }
 }
