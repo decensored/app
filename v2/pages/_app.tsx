dd2eef1901a152a8f59c039c2df802abdb89cb9d
SonOfCrypto <crypto@janwagner-design.de>
2022-01-21 07:55:18 +0100

    added favicon implementation for all browsers and OS

--- v2/pages/_app.tsx
+++ v2/pages/_app.tsx
@@ -10,6 +10,7 @@ const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
     <Helmet>
       <title>Decensored</title>
       <meta name='description' content='Decensored' />
+      <link rel='icon' href='/favicon/favicon.ico' />
       <link
         rel='apple-touch-icon'
         sizes='180x180'
@@ -27,6 +28,14 @@ const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
         sizes='16x16'
         href='/favicon/favicon-16x16.png'
       />
+      <link rel='manifest' href='/favicon/site.webmanifest' />
+      <link
+        rel='mask-icon'
+        href='/favicon/safari-pinned-tab.svg'
+        color='#5bbad5'
+      />
+      <meta name='msapplication-TileColor' content='#da532c' />
+      <meta name='theme-color' content='#ffffff' />
       <body
         className='font-sans bg-slate-100 dark:bg-black
         text-gray-900 dark:text-gray-500'
