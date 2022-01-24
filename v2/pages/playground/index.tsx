f98ef860d1dcbc5e32dfcb35d7e58281a2167711
Eric van Riet Paap <ericvrp@gmail.com>
2022-01-24 23:10:04 +0100

    some minimal chat. not using an external server so only local storage

--- v2/pages/playground/index.tsx
+++ v2/pages/playground/index.tsx
@@ -1,17 +1,54 @@
 import type { NextPage } from 'next'
-import React from 'react'
-import Gun from 'gun'
+import React, { useEffect, useState } from 'react'
+import { classNamesLib } from 'components/ClassNames/ClassNames'
+import GUN from 'gun/gun'
 
-const gun = Gun()
-
-// TODO: Add gun.eco (GunJS) related experiments
+const gun = GUN()
 
 const Playground: NextPage = () => {
-  // console.log(gun)
-  const items = gun.get('items')
+  const [inputText, setInputText] = useState('')
+  const [items, setItems] = useState([])
   // console.log(items)
 
-  return <>{JSON.stringify(items, null, 2)}</>
+  useEffect(
+    () =>
+      gun
+        .get('items')
+        .map()
+        .on((text: string, id: string) =>
+          setItems((oldItems) => [{ text, id }, ...oldItems])
+        ).off,
+    []
+  )
+
+  return (
+    <>
+      <input
+        type='text'
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <br />
+      <button
+        type='button'
+        onClick={() => {
+          gun.get('items').set(inputText as any)
+          setInputText('')
+        }}
+        className={classNamesLib.button}
+      >
+        Submit
+      </button>
+
+      <hr />
+
+      <ul>
+        {Object.values(items).map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default Playground
