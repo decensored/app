744b58f2e6b01103b10ea5f3a185c21bb3928efa
SonOfCrypto <crypto@janwagner-design.de>
2022-01-27 15:36:50 +0100

    added PostDialog component

--- v2/components/Feed/FeedItem.tsx
+++ v2/components/Feed/FeedItem.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent } from 'react'
+import shallow from 'zustand/shallow'
 import Link from 'next/link'
+import useStore from 'lib/store'
 import { faComment, faShare } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { classNamesLib } from 'components/ClassNames/ClassNames'
@@ -20,59 +22,69 @@ const FeedItem: FunctionComponent<FeedItemProps> = ({
   timestamp,
   type,
   spaceName,
-}) => (
-  <div
-    className={`${classNamesLib.feedItemWrapper} ${classNamesLib.feedItemWrapperDark}`}
-  >
-    <div className={classNamesLib.feedItemInnerTop}>
-      <div className={classNamesLib.feedItemMetaWrapper}>
-        <Link href={`/user/${username}`} passHref>
-          <a
-            href='dummy-href'
-            className={`${classNamesLib.feedItemMetaName} ${classNamesLib.feedItemMetaNameDark}`}
-          >
-            {username}
-          </a>
-        </Link>
-        <div className={classNamesLib.feedItemMetaTimestamp}>
-          <TimeAgo date={moment(timestamp).format('YYYY-MM-DD HH:mm:ss')} />
-        </div>
-      </div>
-      <div
-        className={`${classNamesLib.feedItemText} ${classNamesLib.feedItemTextDark}`}
-      >
-        {message}
-      </div>
-    </div>
-    <div className={classNamesLib.feedItemInnerBottom}>
-      <div className={classNamesLib.feedItemInnerBottomCol}>
-        {/*         <FontAwesomeIcon
-          icon={faShieldAlt}
-          className={`${classNamesLib.feedItemInteractionIcon} ${classNamesLib.feedItemInteractionIconDark}`}
-        /> */}
-        {type === 'feed' && (
-          <Link href={`/space/${spaceName}`} passHref>
+}) => {
+  const [setIsOpenPostDialog] = useStore(
+    (state) => [state.setIsOpenPostDialog],
+    shallow
+  )
+
+  return (
+    <div
+      className={`${classNamesLib.feedItemWrapper} ${classNamesLib.feedItemWrapperDark}`}
+    >
+      <div className={classNamesLib.feedItemInnerTop}>
+        <div className={classNamesLib.feedItemMetaWrapper}>
+          <Link href={`/user/${username}`} passHref>
             <a
               href='dummy-href'
-              className={`${classNamesLib.tag} ${classNamesLib.tagClickable} ${classNamesLib.tagClickableDark}`}
+              className={`${classNamesLib.feedItemMetaName} ${classNamesLib.feedItemMetaNameDark}`}
             >
-              #{spaceName}
+              {username}
             </a>
           </Link>
-        )}
+          <div className={classNamesLib.feedItemMetaTimestamp}>
+            <TimeAgo date={moment(timestamp).format('YYYY-MM-DD HH:mm:ss')} />
+          </div>
+        </div>
+        <div
+          className={`${classNamesLib.feedItemText} ${classNamesLib.feedItemTextDark}`}
+        >
+          {message}
+        </div>
       </div>
-      <div className={classNamesLib.feedItemInnerBottomCol}>
-        <FontAwesomeIcon
-          icon={faComment}
-          className={`${classNamesLib.feedItemInteractionIcon} ${classNamesLib.feedItemInteractionIconDark}`}
-        />
-        <FontAwesomeIcon
-          icon={faShare}
-          className={`${classNamesLib.feedItemInteractionIcon} ${classNamesLib.feedItemInteractionIconDark}`}
-        />
+      <div className={classNamesLib.feedItemInnerBottom}>
+        <div className={classNamesLib.feedItemInnerBottomCol}>
+          {/*         <FontAwesomeIcon
+            icon={faShieldAlt}
+            className={`${classNamesLib.feedItemInteractionIcon} ${classNamesLib.feedItemInteractionIconDark}`}
+          /> */}
+          {type === 'feed' && (
+            <Link href={`/space/${spaceName}`} passHref>
+              <a
+                href='dummy-href'
+                className={`${classNamesLib.tag} ${classNamesLib.tagClickable} ${classNamesLib.tagClickableDark}`}
+              >
+                #{spaceName}
+              </a>
+            </Link>
+          )}
+        </div>
+        <div className={classNamesLib.feedItemInnerBottomCol}>
+          <FontAwesomeIcon
+            icon={faComment}
+            className={`${classNamesLib.feedItemInteractionIcon} ${classNamesLib.feedItemInteractionIconDark}`}
+            onClick={() => {
+              setIsOpenPostDialog(true)
+            }}
+          />
+          <FontAwesomeIcon
+            icon={faShare}
+            className={`${classNamesLib.feedItemInteractionIcon} ${classNamesLib.feedItemInteractionIconDark}`}
+          />
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default FeedItem
