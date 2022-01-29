02c2aed8c71965785e85c7196d64ac6dd5048880
christopherturinsky <christopher@myclubs.com>
2022-01-29 21:14:48 +0100

    fix: changed react-helmet to ..-async
    The old package constantly threw an error ;) The console is no like it should be: free of any error

--- v2/pages/_app.tsx
+++ v2/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import React, { FunctionComponent } from 'react'
-import { Helmet } from 'react-helmet'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { toast, ToastContainer } from 'react-toastify'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import 'styles/globals.scss'
@@ -19,36 +19,38 @@ import 'lib/polling/polling_posts'
 
 const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
   <>
-    <Helmet>
-      <title>Decensored</title>
-      <meta name='description' content='Decensored' />
-      <link rel='icon' href='/favicon/favicon.ico' />
-      <link
-        rel='apple-touch-icon'
-        sizes='180x180'
-        href='/favicon/apple-touch-icon.png'
-      />
-      <link
-        rel='icon'
-        type='image/png'
-        sizes='32x32'
-        href='/favicon/favicon-32x32.png'
-      />
-      <link
-        rel='icon'
-        type='image/png'
-        sizes='16x16'
-        href='/favicon/favicon-16x16.png'
-      />
-      <link rel='manifest' href='/favicon/site.webmanifest' />
-      <link
-        rel='mask-icon'
-        href='/favicon/safari-pinned-tab.svg'
-        color='#5bbad5'
-      />
-      <meta name='msapplication-TileColor' content='#da532c' />
-      <meta name='theme-color' content='#ffffff' />
-    </Helmet>
+    <HelmetProvider>
+      <Helmet>
+        <title>Decensored</title>
+        <meta name='description' content='Decensored' />
+        <link rel='icon' href='/favicon/favicon.ico' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/favicon/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/favicon/site.webmanifest' />
+        <link
+          rel='mask-icon'
+          href='/favicon/safari-pinned-tab.svg'
+          color='#5bbad5'
+        />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff' />
+      </Helmet>
+    </HelmetProvider>
 
     <Component {...pageProps} />
 
