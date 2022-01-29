cab138d902e575adeaae284bd956777d2758fd8f
christopherturinsky <christopher@myclubs.com>
2022-01-29 10:10:58 +0100

    feat: make spaceItems clickable

--- v2/components/Spaces/SpaceItem.tsx
+++ v2/components/Spaces/SpaceItem.tsx
@@ -15,49 +15,48 @@ const SpaceItem: FunctionComponent<SpaceItemProps> = ({
   name,
   numberOfPostsInSpace,
 }) => (
-  <div
-    key={id.toString()}
-    className={`${classNamesLib.feedItemWrapper} ${classNamesLib.feedItemWrapperDark}`}
-  >
-    <div className={classNamesLib.feedItemInnerTop}>
-      <div className={classNamesLib.feedItemMetaWrapper}>
-        <Link href={`/space/${name}`} passHref>
-          <a
-            href='dummy-href'
+  <Link href={`/space/${name}`} passHref>
+    <div
+      key={id.toString()}
+      className={`${classNamesLib.feedItemWrapper} ${classNamesLib.feedItemWrapperDark}`}
+    >
+      <div className={classNamesLib.feedItemInnerTop}>
+        <div className={classNamesLib.feedItemMetaWrapper}>
+          <span
             className={`${classNamesLib.feedItemMetaName} ${classNamesLib.feedItemMetaNameDark}`}
           >
             <FontAwesomeIcon icon={faSatellite} className='mr-2' /> {name}
-          </a>
-        </Link>
-        <div
-          className='members flex justify-end items-center
+          </span>
+          <div
+            className='members flex justify-end items-center
          gap-x-2 pointer-events-none'
-        >
-          <FontAwesomeIcon
-            icon={faUserAstronaut}
-            title='member'
-            className='text-md text-gray-900 dark:text-gray-300'
-          />
-          <span
-            className={`
+          >
+            <FontAwesomeIcon
+              icon={faUserAstronaut}
+              title='member'
+              className='text-md text-gray-900 dark:text-gray-300'
+            />
+            <span
+              className={`
             ${classNamesLib.tag}
             ${classNamesLib.tagNotClickable}
             ${classNamesLib.tagNotClickableDark}
           `}
-          >
-            {numberOfPostsInSpace}
-          </span>
+            >
+              {numberOfPostsInSpace}
+            </span>
+          </div>
+        </div>
+        <div
+          className={`${classNamesLib.feedItemText} ${classNamesLib.feedItemTextDark}`}
+        >
+          IOTA is a scalable, decentralized, feeless, modular, open-source
+          distributed ledger protocol that goes beyond blockchain through its
+          core invention of the blockless Tangle.
         </div>
       </div>
-      <div
-        className={`${classNamesLib.feedItemText} ${classNamesLib.feedItemTextDark}`}
-      >
-        IOTA is a scalable, decentralized, feeless, modular, open-source
-        distributed ledger protocol that goes beyond blockchain through its core
-        invention of the blockless Tangle.
-      </div>
     </div>
-  </div>
+  </Link>
 )
 
 export default SpaceItem
