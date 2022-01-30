b472100295aca326b110979f5aa9d93cd50f5857
christopherturinsky <christopher@myclubs.com>
2022-01-30 20:55:54 +0100

    feat: show key after signing up

--- v2/components/Dialog/SignupDialog.tsx
+++ v2/components/Dialog/SignupDialog.tsx
@@ -17,20 +17,16 @@ const SignupDialog: FunctionComponent<SignupDialogProps> = ({
   onClose,
 }) => {
   const [isLoading, setIsLoading] = React.useState(false)
-  const {
-    setIsSignedUp,
-    userName,
-    setUserName,
-    setUserId,
-    contract,
-  } = useStore((state) => ({
-    isSignedUp: state.isSignedUp,
-    setIsSignedUp: state.setIsSignedUp,
-    userName: state.userName,
-    setUserName: state.setUserName,
-    setUserId: state.setUserId,
-    contract: state.contract,
-  }))
+  const [signUpDone, setSignUpDone] = React.useState(false)
+  const { setIsSignedUp, userName, setUserName, setUserId, contract } =
+    useStore((state) => ({
+      isSignedUp: state.isSignedUp,
+      setIsSignedUp: state.setIsSignedUp,
+      userName: state.userName,
+      setUserName: state.setUserName,
+      setUserId: state.setUserId,
+      contract: state.contract,
+    }))
 
   // HANDLE FORM SUBMIT
   type FormValues = {
@@ -50,7 +46,8 @@ const SignupDialog: FunctionComponent<SignupDialogProps> = ({
       setUserName(data.username)
       setUserId(result.userId)
       setIsLoading(false)
-      onClose()
+      setSignUpDone(true)
+      /*     onClose() */
     } else {
       setError(
         'username',
@@ -68,35 +65,62 @@ const SignupDialog: FunctionComponent<SignupDialogProps> = ({
       onClose={onClose}
       header='Signup'
       body={
-        <form id='registerForm' onSubmit={handleSubmit(onSubmit)}>
-          <div className={style.inputWrapper}>
-            <input
-              className={`${style.input} ${style.inputDark}`}
-              type='text'
-              placeholder='Choose your username'
-              defaultValue={userName}
-              {...register('username', {
-                required: true,
-                pattern: /^[A-Za-z]+$/i,
-                min: 4,
-                max: 16,
-              })}
-            />
-            {errors.username && (
-              <div
-                className={`${style.formValidation} ${style.formValidationError}`}
-              >
-                <span
-                  className={`${style.formValidationText} ${style.formValidationTextError}`}
-                >
-                  {errors.username?.type === 'required' &&
-                    'Cant be empty! chars: azAZ'}
-                  {errors.username.message}
-                </span>
+        <>
+          {!signUpDone && (
+            <form id='registerForm' onSubmit={handleSubmit(onSubmit)}>
+              <div className={style.inputWrapper}>
+                <input
+                  className={`${style.input} ${style.inputDark}`}
+                  type='text'
+                  placeholder='Choose your username'
+                  defaultValue={userName}
+                  {...register('username', {
+                    required: true,
+                    pattern: /^[A-Za-z]+$/i,
+                    min: 4,
+                    max: 16,
+                  })}
+                />
+                {errors.username && (
+                  <div
+                    className={`${style.formValidation} ${style.formValidationError}`}
+                  >
+                    <span
+                      className={`${style.formValidationText} ${style.formValidationTextError}`}
+                    >
+                      {errors.username?.type === 'required' &&
+                        'Cant be empty! chars: azAZ'}
+                      {errors.username.message}
+                    </span>
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-        </form>
+            </form>
+          )}
+          {signUpDone && (
+            <div className={style.inputWrapper}>
+              <p className='text-l font-semibold'>Congratulation 🎉</p>
+              <p className='text-sm pt-2'>
+                You successfully signed up on Decensored!
+              </p>
+              <p className='text-sm pt-2'>
+                <b>Important:</b> To login again at a later point or on any
+                other device you will need your key!
+              </p>
+              <input
+                className={`${style.input} ${style.inputDark} mt-5 text-xs`}
+                type='text'
+                defaultValue={localStorage
+                  .getItem('account_private_key')
+                  ?.toString()}
+                disabled
+              />
+              <p className='text-sm pt-2'>
+                Copy your key and store it in a save place!
+              </p>
+            </div>
+          )}
+        </>
       }
       footer={
         <>
@@ -110,23 +134,23 @@ const SignupDialog: FunctionComponent<SignupDialogProps> = ({
             `}
             onClick={() => onClose()}
           >
-            Cancel
+            Close
           </button>
-          <button
-            type='submit'
-            form='registerForm'
-            className={`${style.button} ${style.buttonDecensored} basis-full`}
-          >
-            <span className='whitespace-nowrap'>
-              Sign-up{' '}
-              {isLoading && (
-                <SVGIcon
-                  icon='faSpinner'
-                  className='ml-2 animate-spin'
-                />
-              )}
-            </span>
-          </button>
+
+          {!signUpDone && (
+            <button
+              type='submit'
+              form='registerForm'
+              className={`${style.button} ${style.buttonDecensored} basis-full`}
+            >
+              <span className='whitespace-nowrap'>
+                Sign-up{' '}
+                {isLoading && (
+                  <SVGIcon icon='faSpinner' className='ml-2 animate-spin' />
+                )}
+              </span>
+            </button>
+          )}
         </>
       }
     />
