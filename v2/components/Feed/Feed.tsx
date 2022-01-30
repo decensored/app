052db0180975f4d3a4e1cbdcc4e052c02f493916
Eric van Riet Paap <ericvrp@gmail.com>
2022-01-30 11:56:53 +0100

    remove unneccessary copy of posts state in the feed. Probably this is not what is causing the performance issues

--- v2/components/Feed/Feed.tsx
+++ v2/components/Feed/Feed.tsx
@@ -1,5 +1,4 @@
 import React, { FunctionComponent } from 'react'
-import type { PostType } from 'lib/types'
 import useStore from 'lib/store'
 import { classNamesLib } from 'components/ClassNames/ClassNames'
 import { dequeuePostsAndSpaces } from 'lib/storeUtils'
@@ -11,24 +10,12 @@ const Feed: FunctionComponent = () => {
     posts: state.posts,
     postsQueued: state.postsQueued,
   }))
+  // console.log(`${posts.length} posts`)
 
-  // GET DATA FOR FEED
-  const [feedPosts, setFeedPosts] = React.useState<PostType[]>()
+  const showFeedItems = posts.map((post) => (
+    <FeedItem key={cuid()} type='feed' moderator={false} {...post} />
+  ))
 
-  React.useEffect(() => {
-    setFeedPosts(posts)
-  }, [posts])
-
-  // CHECK IF DATA IS PRESENT AND CREATE FEEDITEMS
-  if (!posts || !feedPosts) {
-    return null
-  }
-  let showFeedItems
-  if (feedPosts.length > 0) {
-    showFeedItems = feedPosts.map((post) => (
-      <FeedItem key={cuid()} type='feed' moderator={false} {...post} />
-    ))
-  }
   return (
     <>
       {postsQueued.length > 0 && (
