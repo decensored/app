62dce39221184560a0abd303437d60cd0132820a
SonOfCrypto <crypto@janwagner-design.de>
2022-01-30 19:10:31 +0100

    added tailwind tailwindcss-children plugin

--- v2/tailwind.config.js
+++ v2/tailwind.config.js
@@ -80,7 +80,8 @@ module.exports = {
     },
   },
   variants: {
-    empty: ['before', 'after','children', 'default', 'children-first', 'children-last', 'children-odd', 'children-even', 'children-not-first', 'children-not-last', 'children-hover', 'hover', 'children-focus', 'focus', 'children-focus-within', 'focus-within', 'children-active', 'active', 'children-visited', 'visited', 'children-disabled', 'disabled', 'responsive'],
+    empty: ['before', 'after'],
+    display: ['children', 'default', 'children-first', 'children-last', 'children-odd', 'children-even', 'children-not-first', 'children-not-last', 'children-hover', 'hover', 'children-focus', 'focus', 'children-focus-within', 'focus-within', 'children-active', 'active', 'children-visited', 'visited', 'children-disabled', 'disabled', 'responsive'],
   },
   plugins: [
     require('tailwindcss-pseudo-elements'),
