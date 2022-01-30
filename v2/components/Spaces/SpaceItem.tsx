ad110d8ff6d248414f8ca5b05acc4430f586062a
christopherturinsky <christopher@myclubs.com>
2022-01-30 17:04:01 +0100

    Add "owner"-tag to spaceItem

--- v2/components/Spaces/SpaceItem.tsx
+++ v2/components/Spaces/SpaceItem.tsx
@@ -2,56 +2,71 @@ import React, { FunctionComponent } from 'react'
 import Link from 'next/link'
 import SVGIcon from 'components/Icon/SVGIcon'
 import { classNamesLib } from 'components/ClassNames/ClassNames'
+import useStore from 'lib/store'
 
 interface SpaceItemProps {
   name: string
+  owner: number
   numberOfPostsInSpace: number
 }
 
 const SpaceItem: FunctionComponent<SpaceItemProps> = ({
   name,
+  owner,
   numberOfPostsInSpace,
-}) => (
-  <Link href={`/space/${name}`} passHref>
-    <div
-      className={`${classNamesLib.feedItemWrapper} ${classNamesLib.feedItemWrapperDark} cursor-pointer`}
-    >
-      <div className={classNamesLib.feedItemInnerTop}>
-        <div className={classNamesLib.feedItemMetaWrapper}>
-          <span
-            className={`${classNamesLib.feedItemMetaName} ${classNamesLib.feedItemMetaNameDark}`}
-          >
-            <SVGIcon icon='faSatellite' className='mr-2' /> {name}
-          </span>
-          <div
-            className='members flex justify-end items-center
-         gap-x-2 pointer-events-none'
-          >
-            <SVGIcon
-              icon='faUserAstronaut'
-              className='text-md text-gray-900 dark:text-gray-300'
-            />
+}) => {
+  const [userId] = useStore((state) => [state.userId])
+  return (
+    <Link href={`/space/${name}`} passHref>
+      <div
+        className={`${classNamesLib.feedItemWrapper} ${classNamesLib.feedItemWrapperDark} cursor-pointer`}
+      >
+        <div className={classNamesLib.feedItemInnerTop}>
+          <div className={classNamesLib.feedItemMetaWrapper}>
             <span
-              className={`
+              className={`${classNamesLib.feedItemMetaName} ${classNamesLib.feedItemMetaNameDark}`}
+            >
+              <div className='flex'>
+                {name}
+                {owner === userId && (
+                  <span
+                    className={`${classNamesLib.tag} ${classNamesLib.tagClickable} cursor-default ml-3`}
+                  >
+                    Owner
+                  </span>
+                )}
+              </div>
+            </span>
+            <div
+              className='members flex justify-end items-center
+         gap-x-2 pointer-events-none'
+            >
+              <SVGIcon
+                icon='faUserAstronaut'
+                className='text-md text-gray-900 dark:text-gray-300'
+              />
+              <span
+                className={`
               ${classNamesLib.tag}
               ${classNamesLib.tagNotClickable}
               ${classNamesLib.tagNotClickableDark}
             `}
-            >
-              {numberOfPostsInSpace}
-            </span>
+              >
+                {numberOfPostsInSpace}
+              </span>
+            </div>
+          </div>
+          <div
+            className={`${classNamesLib.feedItemText} ${classNamesLib.feedItemTextDark}`}
+          >
+            IOTA is a scalable, decentralized, feeless, modular, open-source
+            distributed ledger protocol that goes beyond blockchain through its
+            core invention of the blockless Tangle.
           </div>
         </div>
-        <div
-          className={`${classNamesLib.feedItemText} ${classNamesLib.feedItemTextDark}`}
-        >
-          IOTA is a scalable, decentralized, feeless, modular, open-source
-          distributed ledger protocol that goes beyond blockchain through its
-          core invention of the blockless Tangle.
-        </div>
       </div>
-    </div>
-  </Link>
-)
+    </Link>
+  )
+}
 
 export default SpaceItem
