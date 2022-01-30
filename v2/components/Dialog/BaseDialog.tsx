4efa6b33aeea10d053c8ced02cfe186ecbe990a9
SonOfCrypto <crypto@janwagner-design.de>
2022-01-30 06:21:54 +0100

    refactored SpaceSettingsDialog from global to local state

--- v2/components/Dialog/BaseDialog.tsx
+++ v2/components/Dialog/BaseDialog.tsx
@@ -23,6 +23,7 @@ const BaseDialog: FunctionComponent<DialogProps> = ({
 }) => {
   const widthLookup: { [unit: string]: string } = {
     'sm': 'max-w-sm',
+    'md': 'max-w-md',
     'lg': 'max-w-lg',
     'xl': 'max-w-xl',
     '2xl': 'max-w-2xl',
