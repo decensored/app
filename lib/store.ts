5695500f79fea40c5de66283f4fcb728c2f580a0
Eric van Riet Paap <ericvrp@gmail.com>
2022-01-31 17:10:10 +0100

    just to be sure. increase versions

--- lib/store.ts
+++ lib/store.ts
@@ -2,7 +2,7 @@ import create from 'zustand'
 import { persist } from 'zustand/middleware'
 import type { NodeInfoType, PostType, SpaceType } from 'lib/types'
 
-export const STORE_VERSION = 3
+export const STORE_VERSION = 4
 
 const useStore = create(
   persist(
