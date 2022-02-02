96dedb49e58a39fe9d952387d40ce9dc486061ea
Eric van Riet Paap <ericvrp@gmail.com>
2022-02-02 16:40:23 +0100

    process per batch (currently 25 posts))

--- lib/polling/pollingUtils.ts
+++ lib/polling/pollingUtils.ts
@@ -1,7 +1,7 @@
 import type { PostType, SpaceType } from 'lib/types'
 
 export const pollingConfig = {
-  batchSize: 1000000, // load max this many in parallel from the smartcontract
+  batchSize: 25, // load max this many in parallel from the smartcontract
   highWater: 1250, // when we have at least this...
   lowWater: 1000, // ..reduce to this amount
 }
@@ -11,9 +11,7 @@ export const limitArray = (array: PostType[] | SpaceType[], set: (value: any) =>
 
   if (array.length < pollingConfig.highWater) return // early exit
 
-  console.log(
-    `limitArray ${name} length ${array.length} >= ${pollingConfig.highWater}. Reduce to ${pollingConfig.lowWater}`
-  )
+  console.log(`limit ${name} length ${array.length} >= ${pollingConfig.highWater}. Reduce to ${pollingConfig.lowWater}`)
 
   const newArray = array.slice(0, pollingConfig.lowWater)
   set(newArray)
--- lib/polling/polling_posts.ts
+++ lib/polling/polling_posts.ts
@@ -1,29 +1,32 @@
+/* eslint no-await-in-loop: "off" */ // because we process per batch instead of each
+
 import { inBrowser } from 'lib/where'
 import useStore, { STORE_VERSION } from 'lib/store'
 import { dequeuePostsAndSpaces, nodeIsUpAndRunning } from 'lib/storeUtils'
 import type { LoadingProgressType, PostType } from 'lib/types'
 import { getLatestPostIndex, getPostById } from 'api/feed'
-import { limitArray /* , pollingConfig */ } from './pollingUtils'
+import { limitArray, pollingConfig } from './pollingUtils'
 
 const INTERVAL = 4001 // (first prime over 4000) // 10 * 1000
 
 // XXX these setTimout don't get cancelled during hot code reloading so useTimeout in the next refactor!
 
-const updateStateWithNewPosts = (state: any, newPosts: PostType[]) => {
-  // store newPosts
-  if (state.isPolledDataQueued && state.posts.length) {
-    const allPostsQueued = newPosts.concat(state.postsQueued)
-    state.setPostsQueued(allPostsQueued)
+const updateStateWithNewPosts = (newPosts: PostType[]) => {
+  const state = useStore.getState()
 
-    // console.log(typeof state.userId, state.userId, allPostsQueued)
+  if (state.isPolledDataQueued && state.posts.length) {
+    state.setPostsQueued(newPosts.concat(state.postsQueued).sort((a, b) => b.timestamp - a.timestamp))
 
     // auto-deque when I'm the author of at least one queued post
-    if (allPostsQueued.findIndex((post) => post.author === state.userId) >= 0) {
+    const newState = useStore.getState()
+    if (newState.postsQueued.findIndex((post: PostType) => post.author === newState.userId) >= 0) {
       dequeuePostsAndSpaces()
     }
   } else {
-    const allPosts = newPosts.concat(state.posts)
-    state.setPosts(allPosts)
+    // console.log(
+    //   `isPolledDataQueued=${state.isPolledDataQueued}, nPosts=${state.posts.length}, nNewPosts=${newPosts.length}`
+    // )
+    state.setPosts(newPosts.concat(state.posts).sort((a, b) => b.timestamp - a.timestamp))
   }
 } // end of updateStateWithNewPosts
 
@@ -84,8 +87,8 @@ const poll = async (): Promise<void> => {
 
     // console.log(pollingConfig)
 
-    console.time('polling_posts Loading Batch')
-    const postsPromises: Promise<PostType>[] = []
+    console.time('Loading Posts')
+    let postsPromises: Promise<PostType>[] = []
     for (let i = latestPostIndex; i > state.latestPostIndexFetched; i -= 1) {
       // console.log(`getPostById ${i}`)
       const p = getPostById(contract, i).then((result) => {
@@ -96,16 +99,14 @@ const poll = async (): Promise<void> => {
       })
       postsPromises.push(p)
 
-      // if (postsPromises.length % pollingConfig.batchSize === 0 || i === state.latestPostIndexFetched + 1) {
-      //   console.log(`process batch at ${postsPromises.length}`)
-      //   const newPosts = await Promise.all(postsPromises)
-      //   updateStateWithNewPosts(state, newPosts)
-      //   postsPromises = []
-      // }
+      if (postsPromises.length % pollingConfig.batchSize === 0 || i === state.latestPostIndexFetched + 1) {
+        // console.log(`process batch of ${postsPromises.length} at index ${i}`)
+        const newPosts = await Promise.all(postsPromises)
+        updateStateWithNewPosts(newPosts)
+        postsPromises = []
+      }
     }
-    const newPosts = await Promise.all(postsPromises)
-    updateStateWithNewPosts(state, newPosts)
-    console.timeEnd('polling_posts Loading Batch')
+    console.timeEnd('Loading Posts')
 
     state.setPostsLoaded({ nFinished: 0, max: 0 })
   } // end of latestPostIndex > state.latestPostIndexFetched
