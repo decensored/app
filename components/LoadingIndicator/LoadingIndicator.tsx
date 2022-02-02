7943bc9fe875a8d22f79ed1c16bcee43353d2582
Eric van Riet Paap <ericvrp@gmail.com>
2022-02-02 14:28:23 +0100

    Fix LoadingIndicator which was not receiving updates because of the 'shallow' parameters which caused to to not look for changes inside the postsLoaded and spacesLoaded objects.

--- components/LoadingIndicator/LoadingIndicator.tsx
+++ components/LoadingIndicator/LoadingIndicator.tsx
@@ -1,15 +1,14 @@
 import React, { FunctionComponent } from 'react'
-import shallow from 'zustand/shallow'
 import useStore from 'lib/store'
 import { style } from 'styles/style'
 
 const LoadingIndicator: FunctionComponent = () => {
-  const [postsLoaded, spacesLoaded] = useStore((state) => [state.postsLoaded, state.spacesLoaded], shallow)
+  const [postsLoaded, spacesLoaded] = useStore((state) => [state.postsLoaded, state.spacesLoaded])
 
   const nFinished = postsLoaded.nFinished + spacesLoaded.nFinished
   const max = postsLoaded.max + spacesLoaded.max
 
-  if (nFinished === 0 || nFinished === max || max === 0) return null
+  if (/* nFinished === 0 || */ nFinished === max || max === 0) return null
 
   const percentage = (100 * nFinished) / max
   // console.log(nFinished, max, percentage)
