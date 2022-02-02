1403c554cd6d5ab8767a9a2e78017a77fefdd357
Eric van Riet Paap <ericvrp@gmail.com>
2022-02-02 16:38:30 +0100

    refactor and do not show 'click to see posts' button while loading

--- components/LoadingIndicator/LoadingIndicator.tsx
+++ components/LoadingIndicator/LoadingIndicator.tsx
@@ -1,15 +1,14 @@
 import React, { FunctionComponent } from 'react'
 import useStore from 'lib/store'
 import { style } from 'styles/style'
+import { arePostsOrSpacesLoading } from 'lib/storeUtils'
 
 const LoadingIndicator: FunctionComponent = () => {
   const [postsLoaded, spacesLoaded] = useStore((state) => [state.postsLoaded, state.spacesLoaded])
+  if (!arePostsOrSpacesLoading(postsLoaded, spacesLoaded)) return null
 
   const nFinished = postsLoaded.nFinished + spacesLoaded.nFinished
   const max = postsLoaded.max + spacesLoaded.max
-
-  if (/* nFinished === 0 || */ nFinished === max || max === 0) return null
-
   const percentage = (100 * nFinished) / max
   // console.log(nFinished, max, percentage)
   const s = `Loaded ${percentage.toFixed(1)}%`
