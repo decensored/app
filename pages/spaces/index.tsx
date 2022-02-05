41f9553dc2d409d998e75c619b7a012d5710dc3a
christopherturinsky <christopher@myclubs.com>
2022-02-05 01:35:20 +0100

    feat: add combined search & sort for spaces

--- pages/spaces/index.tsx
+++ pages/spaces/index.tsx
@@ -9,15 +9,25 @@ import SpaceItem from 'components/Spaces/SpaceItem'
 import SpaceHeader from 'components/Spaces/SpaceHeader'
 import { style } from 'styles/style'
 import { sortSpaces } from 'lib/storeUtils'
+import { SpaceType } from 'lib/types'
 
 const Spaces: NextPage = () => {
   const [isSignedUp, spaces, posts] = useStore((state) => [state.isSignedUp, state.spaces, state.posts], shallow)
-
-  // sort spaces
   const [sortType, setSortType] = React.useState('numberOfPostsInSpace')
-  const createSpaceItems = sortSpaces(spaces, posts, sortType).map((space) => (
-    <SpaceItem key={`space-${space.id}`} {...space} />
-  ))
+  const [searchTerm, setSearchTerm] = React.useState('')
+  const [spaceResults, setSpaceResults] = React.useState([] as SpaceType[])
+
+  const handleChange = (event: any) => {
+    setSearchTerm(event.target.value)
+  }
+
+  // Search or sort spaces
+  React.useEffect(() => {
+    const result = sortSpaces(spaces, posts, sortType).filter((space) => space.name.toLowerCase().includes(searchTerm))
+    setSpaceResults(result)
+  }, [searchTerm, sortType, spaces, posts])
+
+  const createSpaceItems = spaceResults.map((space: any) => <SpaceItem key={`space-${space.id}`} {...space} />)
 
   return (
     <>
@@ -29,21 +39,35 @@ const Spaces: NextPage = () => {
         <div className={style.bodyContainerCol2}>
           <div className={style.feedWrapper}>
             {isSignedUp && <SpaceHeader />}
-            <select
-              className={`
+            <div className='flex'>
+              <input
+                value={searchTerm}
+                onChange={handleChange}
+                type='text'
+                placeholder='Search for a space'
+                className={`
                 ${style.input}
                 ${style.inputDefault}
                 ${style.inputDefaultDark}
                 ${style.inputFocus}
               `}
-              onChange={(e) => setSortType(e.target.value)}
-            >
-              <option value='numberOfPostsInSpace|desc'>Most active</option>
-              <option value='latestPostIndexInSpace|desc'>Latest Activity</option>
-              <option value='id|desc'>Newest Space</option>
-              {/*             <option value='name|asc'>Name A-Z</option>
+              />
+              <select
+                className={`
+                ${style.input}
+                ${style.inputDefault}
+                ${style.inputDefaultDark}
+                ${style.inputFocus}
+              `}
+                onChange={(e) => setSortType(e.target.value)}
+              >
+                <option value='numberOfPostsInSpace|desc'>Most active</option>
+                <option value='latestPostIndexInSpace|desc'>Latest Activity</option>
+                <option value='id|desc'>Newest Space</option>
+                {/*             <option value='name|asc'>Name A-Z</option>
               <option value='name|desc'>Name Z-A</option> */}
-            </select>
+              </select>
+            </div>
             {createSpaceItems}
           </div>
         </div>
