3e726a4028b5c9bf6ec443bfad1dcdfae5901c19
Sundeamon <heroku-smk@outlook.com>
2022-02-06 16:54:24 +0100

    (fix) define type for the store and assign it to store creation
    This fixes the type issues when accessing the store via useStore hook.

--- lib/store.ts
+++ lib/store.ts
@@ -1,84 +1,128 @@
-import create from 'zustand'
-import { persist } from 'zustand/middleware'
+import create, { GetState, SetState } from 'zustand'
+import { persist, StoreApiWithPersist } from 'zustand/middleware'
 import type { LoadingProgressType, NodeInfoType, PostType, SpaceType } from 'lib/types'
 
 export const STORE_VERSION = 11
 
 export const DEFAULT_EVMNODE = 'https://hh.addiota.com'
+
 export const DEFAULT_CONTRACTSADDRESS = '0x3eb8De6C1D7d920fc72f0745475Ecf37a0cF3BF3'
 // main:  0x3421b655B1Be5Cd7F2466c4A9139a416b2148fe0
 
+export type StateType = {
+  storeVersion: number
+  setStoreVersion: (storeVersion: number) => void
+  isSignedUp: boolean
+  setIsSignedUp: (isSignedUp: boolean) => void
+  isPolledDataQueued: boolean
+  setIsPolledDataQueued: (isPolledDataQueued: boolean) => void
+  contract: any
+  setContract: (contract: any) => void
+  postsLoaded: LoadingProgressType
+  setPostsLoaded: (postsLoaded: LoadingProgressType) => void
+  spacesLoaded: LoadingProgressType
+  setSpacesLoaded: (spacesLoaded: LoadingProgressType) => void
+  latestAccountIndexFetched: number
+  setLatestAccountIndexFetched: (latestAccountIndexFetched: number) => void
+  spaces: SpaceType[]
+  setSpaces: (spaces: SpaceType[]) => void
+  spacesQueued: SpaceType[]
+  setSpacesQueued: (spacesQueued: SpaceType[]) => void
+  latestSpaceIndexFetched: number
+  setLatestSpaceIndexFetched: (latestSpaceIndexFetched: number) => void
+  spacesSortType: string
+  setSpacesSortType: (spacesSortType: string) => void
+  posts: PostType[]
+  setPosts: (posts: PostType[]) => void
+  postsQueued: PostType[]
+  setPostsQueued: (postsQueued: PostType[]) => void
+  latestPostIndexFetched: number
+  setLatestPostIndexFeched: (latestPostIndexFetched: number) => void
+  isDarkmode: boolean
+  setIsDarkmode: (isDarkmode: boolean) => void
+  userName: string
+  setUserName: (userName: string) => void
+  privateKey: string
+  setPrivateKey: (privateKey: string) => void
+  userId: number
+  setUserId: (userId: number) => void
+  defaultNodeInfo: boolean
+  setDefaultNodeInfo: (defaultNodeInfo: boolean) => void
+  nodeInfo: NodeInfoType
+  setNodeInfo: (nodeInfo: NodeInfoType) => void
+  cacheFlush: () => void
+}
+
 const useStore = create(
-  persist(
+  persist<StateType, SetState<StateType>, GetState<StateType>, StoreApiWithPersist<StateType>>(
     (set) => ({
       storeVersion: STORE_VERSION,
-      setStoreVersion: (storeVersion: number) => set({ storeVersion }),
+      setStoreVersion: (storeVersion) => set({ storeVersion }),
 
       isSignedUp: false,
-      setIsSignedUp: (isSignedUp: boolean) => set({ isSignedUp }),
+      setIsSignedUp: (isSignedUp) => set({ isSignedUp }),
 
       isPolledDataQueued: true, // note: looks like this setting is no longer changing
-      setIsPolledDataQueued: (isPolledDataQueued: boolean) => set({ isPolledDataQueued }),
+      setIsPolledDataQueued: (isPolledDataQueued) => set({ isPolledDataQueued }),
 
       // smart contracts
       contract: {}, // { accounts, posts, spaces, web3 },
-      setContract: (contract: any) => set({ contract }),
+      setContract: (contract) => set({ contract }),
 
       postsLoaded: { nFinished: 0, max: 0 } as LoadingProgressType,
-      setPostsLoaded: (postsLoaded: LoadingProgressType) => set({ postsLoaded }),
+      setPostsLoaded: (postsLoaded) => set({ postsLoaded }),
 
       spacesLoaded: { nFinished: 0, max: 0 } as LoadingProgressType,
-      setSpacesLoaded: (spacesLoaded: LoadingProgressType) => set({ spacesLoaded }),
+      setSpacesLoaded: (spacesLoaded) => set({ spacesLoaded }),
 
       // ACCOUNTS
       latestAccountIndexFetched: 0,
-      setLatestAccountIndexFetched: (latestAccountIndexFetched: number) => set({ latestAccountIndexFetched }),
+      setLatestAccountIndexFetched: (latestAccountIndexFetched) => set({ latestAccountIndexFetched }),
 
       // SPACES
       spaces: [] as SpaceType[],
-      setSpaces: (spaces: SpaceType[]) => set({ spaces }),
+      setSpaces: (spaces) => set({ spaces }),
 
       spacesQueued: [] as SpaceType[],
-      setSpacesQueued: (spacesQueued: SpaceType[]) => set({ spacesQueued }),
+      setSpacesQueued: (spacesQueued) => set({ spacesQueued }),
 
       latestSpaceIndexFetched: 0,
-      setLatestSpaceIndexFetched: (latestSpaceIndexFetched: number) => set({ latestSpaceIndexFetched }),
+      setLatestSpaceIndexFetched: (latestSpaceIndexFetched) => set({ latestSpaceIndexFetched }),
 
       spacesSortType: 'numberOfPostsInSpace|desc',
-      setSpacesSortType: (spacesSortType: string) => set({ spacesSortType }),
+      setSpacesSortType: (spacesSortType) => set({ spacesSortType }),
       // POSTS
       posts: [] as PostType[],
-      setPosts: (posts: PostType[]) => set({ posts }),
+      setPosts: (posts) => set({ posts }),
 
       postsQueued: [] as PostType[],
-      setPostsQueued: (postsQueued: PostType[]) => set({ postsQueued }),
+      setPostsQueued: (postsQueued) => set({ postsQueued }),
 
       latestPostIndexFetched: 0,
-      setLatestPostIndexFeched: (latestPostIndexFetched: number) => set({ latestPostIndexFetched }),
+      setLatestPostIndexFeched: (latestPostIndexFetched) => set({ latestPostIndexFetched }),
 
       // VARIOUS
       isDarkmode: false,
-      setIsDarkmode: (isDarkmode: boolean) => set({ isDarkmode }),
+      setIsDarkmode: (isDarkmode) => set({ isDarkmode }),
 
       userName: '',
-      setUserName: (userName: string) => set({ userName }),
+      setUserName: (userName) => set({ userName }),
 
       privateKey: '',
-      setPrivateKey: (privateKey: string) => set({ privateKey }),
+      setPrivateKey: (privateKey) => set({ privateKey }),
 
       userId: 0,
-      setUserId: (userId: number) => set({ userId }),
+      setUserId: (userId) => set({ userId }),
 
       defaultNodeInfo: true,
-      setDefaultNodeInfo: (defaultNodeInfo: boolean) => set({ defaultNodeInfo }),
+      setDefaultNodeInfo: (defaultNodeInfo) => set({ defaultNodeInfo }),
 
       nodeInfo: {
         evmNode: DEFAULT_EVMNODE,
         contractsAddress: DEFAULT_CONTRACTSADDRESS,
       } as NodeInfoType,
-      setNodeInfo: (nodeInfo: NodeInfoType) => {
-        set({ nodeInfo })
-      },
+      setNodeInfo: (nodeInfo) => set({ nodeInfo }),
+
       cacheFlush: () => {
         // console.log(`cacheFlush to storeVersion ${STORE_VERSION}`)
         set({
@@ -94,6 +138,7 @@ const useStore = create(
       },
     }),
     {
+      // Configure persist middleware
       name: 'decensored',
 
       partialize: (state) => ({
