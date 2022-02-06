e428f804bb5e00ac588e03d1f9208cc5eb8e91be
Eric van Riet Paap <ericvrp@gmail.com>
2022-02-06 20:01:47 +0100

    remove warning about state

--- lib/polling/polling_accounts.ts
+++ lib/polling/polling_accounts.ts
@@ -2,10 +2,10 @@ import { runPoller } from './pollingUtils'
 
 // import { getLatestAccountIndex } from 'api/user'
 
-const INTERVAL = 9973 // highest prime below 10000
+const INTERVAL = 10000 * 9973 // highest prime below 10000
 
 const pollAccounts = async (state: any): Promise<void> => {
-  //   console.log('pollAccounts storeVersion', state.storeVersion)
+  console.log('pollAccounts storeVersion', state.storeVersion)
   //   const latestPostIndex = await getLatestAccountIndex(state.contract)
   //   if (latestPostIndex === state.latestPostIndexFetched) return
   //   state.setLatestAccountIndexFeched(latestPostIndex)
