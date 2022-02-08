070c80b96545621c74b765592b32e9d0707ee2e6
christopherturinsky <christopher@myclubs.com>
2022-02-08 19:13:42 +0100

    fix: use regex on hashtags for trending overview
    Right now multiple "same" hashtags will show up in trending depending on lower/Uppercase

--- lib/storeUtils.ts
+++ lib/storeUtils.ts
@@ -97,13 +97,14 @@ export const getPostsWithHashtag = (posts: PostType[], hashtag: string): PostTyp
 
 export const getTrendingHashtags = (posts: PostType[], hours: number, min: number): any => {
   const tags: string[] = []
+  const regex = /[^a-z1-9]/g
   posts
     .filter((post) => post.message.includes(`#`) && post.timestamp * 1000 >= +new Date() - hours * 3600 * 1000)
     .forEach((post) => {
       post.message
         .split(' ')
         .filter((part: string) => part.startsWith('#'))
-        .map((tag) => tags.push(tag.replace('#', '')))
+        .map((tag) => tags.push(tag.toLocaleLowerCase().replace(regex, '')))
     })
 
   const count = {} as any
